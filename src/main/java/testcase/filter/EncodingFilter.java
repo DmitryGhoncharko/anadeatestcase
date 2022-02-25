0279590419c764b2e2fbdc45482f5a459fa0537f
Acer <dafae722@gmail.com>
2022-02-26 00:45:39 +0300

    refactor code

--- src/main/java/testcase/filter/EncodingFilter.java
+++ src/main/java/testcase/filter/EncodingFilter.java
@@ -4,13 +4,14 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebInitParam;
 import java.io.IOException;
+
 @WebFilter(filterName = "encodingFilter")
 @WebInitParam(name = "requestEncoding", value = "UTF-8")
 public class EncodingFilter implements Filter {
 
     private String encoding;
 
-    public void init(FilterConfig config) throws ServletException {
+    public void init(FilterConfig config) {
         encoding = config.getInitParameter("requestEncoding");
         if (encoding == null) encoding = "UTF-8";
     }
